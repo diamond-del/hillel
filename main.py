1b36ac6c0a5f0837935940e1120f8dd9ba5144cf
Evgen <vodkafigna@gmail.com>
2024-08-20 18:35:44 +0300

    fixed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from utils import email_sender
 
-print('From main', __name__)
+print("From main", __name__)
+
+
 def main():
-    email_sender.send_email(
-        recipients=['evgeny_saprykin@ukr.net'],
-        mail_body='Yo check this pic',
-        mail_subject='Email'
-    )
+    email_sender.send_email(recipients=["evgeny_saprykin@ukr.net"], mail_body="Yo check this pic", mail_subject="Email")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utils/email_sender.py
+++ utils/email_sender.py
@@ -1,37 +1,39 @@
-import config
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import os
+import smtplib
+from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.image import MIMEImage
-from email import encoders
-import smtplib
-import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import config
+
 
 def send_email(
-        recipients: list[str],
-        mail_body: str,
-        mail_subject: str,
-        attachments: str = None,
-)-> None:
-    print('From sender', __name__)
+    recipients: list[str],
+    mail_body: str,
+    mail_subject: str,
+    attachments: str = None,
+) -> None:
+    print("From sender", __name__)
 
     TOKEN_API = config.TOKEN_API
     USER = config.USER
     SMTP_SERVER = config.SMTP_SERVER
 
-    image_path = 'pic.png'
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = mail_subject
-    msg['From'] = f'{USER} sent this email'
-    msg['To'] = ', '.join(recipients)
-    msg['Reply-to'] = USER
-    msg['Return-Path'] = USER
-    msg['X-Mailer'] = 'decorator'
+    image_path = "pic.png"
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = mail_subject
+    msg["From"] = f"{USER} sent this email"
+    msg["To"] = ", ".join(recipients)
+    msg["Reply-to"] = USER
+    msg["Return-Path"] = USER
+    msg["X-Mailer"] = "decorator"
 
-    text_to_send = MIMEText(mail_body, 'plain')
+    text_to_send = MIMEText(mail_body, "plain")
     msg.attach(text_to_send)
 
-    with open(image_path, 'rb') as f:
+    with open(image_path, "rb") as f:
         img = MIMEImage(f.read(), name=os.path.basename(image_path))
         msg.attach(img)
 
