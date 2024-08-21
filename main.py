ed12ea6a80c07f3bf64948815265ccab6daa9c68
Evgen <vodkafigna@gmail.com>
2024-08-21 12:20:32 +0300

    car's class

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from utils import email_sender
+class Car:
+    def __init__(self, model, year, cost):
+        self.model = model
+        self.year = year
+        self.cost = cost
 
-print("From main", __name__)
+    def drive(self, distance):
+        depreciation = 10 * distance
+        self.cost -= depreciation
+        if self.cost < 0:
+            self.cost = 0
+
+    @property
+    def category(self):
+        if self.cost > 10_000_000:
+            return "Елітне"
+        elif 2_000_000 <= self.cost <= 10_000_000:
+            return "Середнячок"
+        else:
+            return "Економ"
+
+    def __str__(self):
+        return f"Модель: {self.model}, Рік: {self.year}, Вартість: {self.cost} грн, Категорія: {self.category}"
 
 
-def main():
-    email_sender.send_email(recipients=["evgeny_saprykin@ukr.net"], mail_body="Yo check this pic", mail_subject="Email")
+car = Car("Tesla Model S", 2022, 12_000_000)
+print(car)
 
-
-if __name__ == "__main__":
-    main()
+car.drive(2000)
+print(car)
